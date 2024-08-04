d789a37e084beebbeacbadbf9b25a234d3d05c58
Victor Mazo <viamazoba@unal.edu.co>
2024-08-03 21:56:30 -0500

    feat: Se finaliza configuración de variables de entorno.

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
+// Esta librería lleva las variables de entorno a las de node
+// Así se pueden utilizar en cuelaquier lugar
+const dotenv = require('dotenv');
+dotenv.config({
+    path: './config.env'
+});
+
 const app = require('./app');
 const colors = require('colors');
 
-const port = 3000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
     console.log(colors.blue.bold(`App runing on port ${port}...`))
 })
