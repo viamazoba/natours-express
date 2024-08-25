8c2e18e49fd86f694f7e2329e3920667b00f281b
Victor Mazo <viamazoba@unal.edu.co>
2024-08-25 17:55:29 -0500

    feat: Se implementa filtado inicial de los tours.

--- controllers/tour.controller.js
+++ controllers/tour.controller.js
@@ -3,10 +3,25 @@ const Tour = require('../models/tourModel');
 
 
 exports.getAllTours = async (req, res) => {
+	const queryObj = {
+		...req.query
+	};
+
+	const excludeFields = ['page', 'sort', 'limit', 'fields'];
+	excludeFields.forEach(el => delete queryObj[el])
 
 	try {
 
-		const tours = await Tour.find();
+		// Built the query
+
+		const query = Tour.find({
+			...queryObj
+		});
+
+		// Execute Query
+		const tours = await query;
+
+		// Send Response
 		res.status(200).json({
 			status: 'success',
 			results: tours.length,
