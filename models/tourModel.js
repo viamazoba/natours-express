7b7ab5c05244d2fdd414b4776b516876d7fa2146
Victor Mazo <viamazoba@unal.edu.co>
2024-08-31 04:31:08 -0500

    feat: Se implementa pre middleware aggregation al proyecto.

--- models/tourModel.js
+++ models/tourModel.js
@@ -103,6 +103,16 @@ tourSchema.pre('find', function (next) {
     next();
 })
 
+// Aggregation Middleware
+tourSchema.pre('aggregate', function (next) {
+    this.pipeline().unshift({
+        $match: {
+            secretTour: { $ne: true }
+        }
+    });
+
+    next();
+})
 
 const Tour = mongoose.model('Tour', tourSchema);
 
