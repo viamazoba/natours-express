6d4e1091030a23ca57a668c485abb7f5cd47aa35
Victor Mazo <viamazoba@unal.edu.co>
2024-08-31 19:51:02 -0500

    feat: Se implementa manejador global de errores.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 const express = require('express');
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
@@ -30,9 +31,27 @@ app.use('/api/v1/users', userRouter);
 
 // Este middleware es para enviar mensaje cuando se introduce un endpoint incorrecto
 app.all('*', (req, res, next) => {
-	res.status(404).json({
-		status: 'fail',
-		message: `Can't find ${req.originalUrl} on this server!`
+	// res.status(404).json({
+	// 	status: 'fail',
+	// 	message: `Can't find ${req.originalUrl} on this server!`
+	// });
+
+	const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+	err.status = 'fail';
+	err.statusCode = 404;
+
+	next(err);
+});
+
+// Manejador global de errores
+// Este middleware maneja todos los errores de la aplicación
+app.use((err, req, res, next) => {
+	err.statusCode = err.statusCode || 500;
+	err.status = err.status || 'error';
+
+	res.status(err.statusCode).json({
+		status: err.status,
+		message: err.message
 	});
 });
 
