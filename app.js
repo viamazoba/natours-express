72fa3003c6e896ea0cdd56116b283576582344ce
Victor Mazo <viamazoba@unal.edu.co>
2024-09-01 03:17:03 -0500

    feat: Se crea clase para manejar errores, además se refactoriza el middleware para manejar errores globalmente.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-param-reassign */
 const express = require('express');
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/error.controller');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -31,28 +33,11 @@ app.use('/api/v1/users', userRouter);
 
 // Este middleware es para enviar mensaje cuando se introduce un endpoint incorrecto
 app.all('*', (req, res, next) => {
-	// res.status(404).json({
-	// 	status: 'fail',
-	// 	message: `Can't find ${req.originalUrl} on this server!`
-	// });
-
-	const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-	err.status = 'fail';
-	err.statusCode = 404;
-
-	next(err);
+	next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
 // Manejador global de errores
 // Este middleware maneja todos los errores de la aplicación
-app.use((err, req, res, next) => {
-	err.statusCode = err.statusCode || 500;
-	err.status = err.status || 'error';
-
-	res.status(err.statusCode).json({
-		status: err.status,
-		message: err.message
-	});
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
---
+++ controllers/error.controller.js
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-param-reassign */
+module.exports = (err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+    });
+};
