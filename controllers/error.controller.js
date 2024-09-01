2e577c133d562e99f552edeb0bbf369ce8978903
Victor Mazo <viamazoba@unal.edu.co>
2024-09-01 16:24:39 -0500

    feat: Se actualiza manejo de errores para desarrollo y para producción.

--- controllers/error.controller.js
+++ controllers/error.controller.js
@@ -1,11 +1,32 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable no-param-reassign */
-module.exports = (err, req, res, next) => {
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error';
 
+// Como se debe mostrar un error mientras desarrollas
+const sendErrorDev = (err, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack
+    });
+};
+
+// Como se debe mostrar un error en producción
+const sendErrorProd = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
         message: err.message
     });
+};
+
+module.exports = (err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+
+    if (process.env.NODE_ENV === 'development') {
+        sendErrorDev(err, res);
+
+    } else if (process.env.NODE_ENV === 'production') {
+        sendErrorProd(err, res);
+    }
 };
